13dad9b70dcd55fd8fecafb1977cfc6e6f09f4d7
GeneralGrifon <56627540+GeneralGrifon@users.noreply.github.com>
2022-02-06 21:54:40 +0400

    Add files via upload

---
+++ actual.js
@@ -0,0 +1,44 @@
+$('.gadjet').on("click", function() {
+          $("#gadjet").toggleClass('gdjton'); 
+        });
+
+
+$(document).ready(function(){
+    $('#z2').on("click", function() {
+    $('.petportal').addClass('ppoff');
+    });
+    });
+
+$(document).ready(function(){
+    $('#z6').on("click", function() {
+    $('.petportal').addClass('ppoff');
+    });
+    });
+
+$(document).ready(function(){
+    $('#show').on("click", function() {
+    $('.petportal').addClass('ppoff');
+    });
+    });
+
+$(document).ready(function(){
+    $('.opener').on("click", function() {
+    $('.chatiframe').toggleClass('chaton');
+    $('.blur').removeClass('poston2');
+    $('.blur').removeClass('poston');
+    $('.smart2').removeClass('active8');
+    });
+    });
+
+$(document).ready(function(){
+        $('.manifest').on("mouseenter mouseleave", function() {
+        $('.manifest').toggleClass('active9');  
+        });
+        });
+
+
+
+
+
+
+    
