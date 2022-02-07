5d7deac384deae30b7929ab187613bfa9628787c
GeneralGrifon <56627540+GeneralGrifon@users.noreply.github.com>
2022-02-07 20:05:16 +0400

    Update user.js

--- user.js
+++ user.js
@@ -518,3 +518,14 @@ function menuoff()
     }
 
 
+// function DOPEMODE2() {
+//             var o = $(".scene");
+
+//     $("#card").on("mousemove", function (t) {
+//         var e = -($(window).innerWidth() / 2 - t.pageX) / 20,
+//             n = ($(window).innerHeight() / 2 - t.pageY) / 20;
+//         o.attr("style", "transform: rotateY(" + e + "deg) rotateX(" + n + "deg);-webkit-transform: rotateY(" + e + "deg) rotateX(" + n + "deg);-moz-transform: rotateY(" + e + "deg) rotateX(" + n + "deg)")
+
+//     })   
+// }
+
